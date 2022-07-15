4d5735609048bc666dcc713259d79b57018203c3
LuckerChar <kokakolka85@gmail.com>
2022-07-15 16:19:55 +0300

    new

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,25 +1,23 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDao;
-import web.model.Car;
+import web.service.CarService;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private final CarDao carDao;
+    private final CarService carService;
 
-    public CarController(CarDao carDao) {
-        this.carDao = carDao;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
     @GetMapping()
-    public String cars(@RequestParam(value = "count", defaultValue = "5")int a, Model model) {
-        model.addAttribute("cars", carDao.getCars(a));
+    public String cars(@RequestParam(value = "count", defaultValue = "5")int count, Model model) {
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
--- src/main/java/web/dao/CarDao.java
+++ src/main/java/web/dao/CarDao.java
@@ -1,27 +1,17 @@
-package web.dao;
-
-import org.springframework.stereotype.Component;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
-public class CarDao {
-    private static int CARS_COUNT;
-
-    private List<Car> cars = new ArrayList<>();
-
-    {
-        cars = new ArrayList<>();
-        cars.add(new Car(++CARS_COUNT, "BMW", "z1", 2));
-        cars.add(new Car(++CARS_COUNT, "Mers", "z2", 4));
-        cars.add(new Car(++CARS_COUNT, "Kia", "z3", 2));
-        cars.add(new Car(++CARS_COUNT, "Honda", "z4", 4));
-        cars.add(new Car(++CARS_COUNT, "Ford", "z5", 2));
-    }
-
-    public List<Car> getCars(int a) {
-        return a < 5 ? cars.stream().limit(a).toList(): cars;
-        }
-    }
+//package web.dao;
+//
+//import org.springframework.stereotype.Component;
+//import web.model.Car;
+//import web.service.CarService;
+//
+//import java.util.List;
+//
+//@Component
+//public class CarDao {
+//    private CarDao carDao;
+//    private CarService carService;
+//
+//    public List<Car> getCarList(int count) {
+//        return carDao.g(count);
+//    }
+//}
